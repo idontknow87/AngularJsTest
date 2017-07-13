c46b39ea3e90f8f5ca461ad33e82eab39ed8d073
Marc Mempin <marc.mempin@perfectchannel.com>
2017-07-13 16:07:55 +0100

    add files

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+angular.module('app', []);
+
+
+require('./components/home/home.component');
+require('./components/dealDetail/dealDetail.component');
+import './styles.scss';
+
