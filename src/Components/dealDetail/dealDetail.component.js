65c0c69a101fd81a9632b14027d0573a9e6dbc8e
Marc Mempin <marc.mempin@perfectchannel.com>
2017-07-13 17:24:54 +0100

    added left column and header

--- src/Components/dealDetail/dealDetail.component.js
+++ src/Components/dealDetail/dealDetail.component.js
@@ -1,26 +1,8 @@
 import angular from 'angular';
 
-
 angular.module('app').component('dealDetail', {
 	templateUrl: '/templates/dealDetail.html',
 	bindings: {
 		deals: '<'    
 	}
 });
-
-angular.module('app').component('dealDetailRow', {
-	templateUrl: '/templates/dealDetailRow.html',
-	controller: dealDetailComponentController,
-	restrict: 'A',
-	replace: false,
-	bindings: {
-		deal: '<'    
-	}
-});
-
-function dealDetailComponentController($filter){	
- 	var ctrl = this;
-	ctrl.GetTotalContractCost = function() {
-		return $filter('currency')(ctrl.deal.prices[0].totalContractCost,"£");
-	};	
-}
--- src/Components/home/home.component.js
+++ src/Components/home/home.component.js
@@ -15,21 +15,26 @@ homeService.$inject = ['$http']
 
 
 angular.module('app').component('home', {
-	template: `
-	<div><a href="/"  id="logo"></a> 
-<svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
-
-<deal-detail deals="$ctrl.deals">
- </deal-detail>
-
-</div>
-	
-	`,
+	templateUrl: '/templates/home.html',
 	controller: homeComponentController
 })
 
 function homeComponentController(homeService){
-	this.deals = [];
-	homeService.getDeals().then(deals=>this.deals=deals)
+	var ctrl = this;
+	ctrl.deals = [];
+	homeService.getDeals().then(deals=>ctrl.deals=deals);
+
+	ctrl.GetProductTypeFilters = function() {
+		var result =[];
+		for (var t = 0; t < ctrl.deals.length; t++) {
+			for (var i = 0; i < ctrl.deals[t].productTypes.length; i++) {
+				if(!result.includes(ctrl.deals[t].productTypes[i])){
+					result.push(ctrl.deals[t].productTypes[i]);
+				}
+			}
+		}
+		return result;
+	};	
 }
+
 homeComponentController.$inject = ['home.service']
